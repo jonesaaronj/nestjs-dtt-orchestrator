b73f98aa81b999ac31de2a64abafcc6b0d5c4463
Aaron Jones <aaron.jones@accretivetg.com>
2026-03-12 11:15:43 -0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
